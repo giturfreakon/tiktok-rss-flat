91f427c7ca793a078e056eff265bb80af7483c65
Piper Sparrow (she/her) | SparkliTwizzl <github@sparklitwizzl.com>
2023-04-05 02:02:55 -0600

    fix typecasting bug

--- postprocessing.py
+++ postprocessing.py
@@ -18,85 +18,85 @@ subscriptionFileCount = 4
 
 async def runAll():
 	try:
-		print('-------------------- runAll start --------------------')
+		print( '-------------------- runAll start --------------------' )
 		
 		currentFileNumber = 0
-		print(f'-------------------- attempt to load subscription file number to run --------------------')
-		with open('nextSubscriptionFileToRun.py') as f:
+		print( f'-------------------- attempt to load subscription file number to run --------------------' )
+		with open( 'nextSubscriptionFileToRun.py' ) as f:
 			currentFileNumber = f.read()
-			print(f'-------------------- currentFileNumber = {currentFileNumber} --------------------')
+			print( f'-------------------- currentFileNumber = {currentFileNumber} --------------------' )
 		
 		fileToRun = 'subscriptions' + currentFileNumber + '.csv'
-		print(f'-------------------- fileToRun = {fileToRun} --------------------')
+		print( f'-------------------- fileToRun = {fileToRun} --------------------' )
 		
-		with open(fileToRun) as f:
+		with open( fileToRun ) as f:
 			# TODO: Switch to 3.11 TaskGroup or trio nursery
-			await asyncio.gather(*[
-				run(row['username']) for row in csv.DictReader(f, fieldnames=['username'])])
+			await asyncio.gather( *[
+				run( row['username'] ) for row in csv.DictReader( f, fieldnames=['username'] ) ] )
 		
-		nextFileNumber = int(currentFileNumber) + 1
-		if (nextFileNumber > subscriptionFileCount):
+		nextFileNumber = int( currentFileNumber ) + 1
+		if ( nextFileNumber > subscriptionFileCount ):
 			nextFileNumber = 1
-		print(f'-------------------- nextFileNumber = {nextFileNumber} --------------------')
+		print( f'-------------------- nextFileNumber = {nextFileNumber} --------------------' )
 		
-		print(f'-------------------- attempt to store next subscription file number to run \(subscriptions{nextFileNumber}.csv\) --------------------')
-		with open('nextSubscriptionFileToRun.py') as f:
-			f.write(nextFileNumber)
+		print( f'-------------------- attempt to store next subscription file number to run \( subscriptions{nextFileNumber}.csv\ ) --------------------' )
+		with open( 'nextSubscriptionFileToRun.py' ) as f:
+			f.write( str( nextFileNumber ) )
 	except Exception as e:
-		print(f'-------------------- an error occurred: {e} --------------------')
+		print( f'-------------------- an error occurred: {e} --------------------' )
 	
-	print('-------------------- runAll end --------------------')
+	print( '-------------------- runAll end --------------------' )
 
 
-async def run(csvuser):
-	print(f'-------------------- run start {csvuser} --------------------')
+async def run( csvuser ):
+	print( f'-------------------- run start {csvuser} --------------------' )
 	try:		
 		fg = FeedGenerator()
-		fg.id('https://tiktok.com/@' + csvuser)
-		fg.title('@' + csvuser + ' | TikTok')
+		fg.id( 'https://tiktok.com/@' + csvuser )
+		fg.title( '@' + csvuser + ' | TikTok' )
 		fg.author( {'name':'Conor ONeill','email':'conor@conoroneill.com'} )
 		fg.link( href='http://tiktok.com', rel='alternate' )
-		fg.logo(ghPagesURL + 'tiktok-rss.png')
-		fg.subtitle('Latest TikToks from @' + csvuser)
+		fg.logo( ghPagesURL + 'tiktok-rss.png' )
+		fg.subtitle( 'Latest TikToks from @' + csvuser )
 		fg.link( href=ghPagesURL + 'rss/' + csvuser + '.xml', rel='self' )
-		fg.language('en')
+		fg.language( 'en' )
 		
 		# Set the last modification time for the feed to be the most recent post, else now.
 		updated=None
 		
-		async with AsyncTikTokAPI(navigation_retries=3, navigation_timeout=60) as api:
-			tiktokuser = await api.user(csvuser, video_limit=maxItems)
+		async with AsyncTikTokAPI( navigation_retries=3, navigation_timeout=60 ) as api:
+			tiktokuser = await api.user( csvuser, video_limit=maxItems )
 			async for video in tiktokuser.videos:
-				# print(video.create_time, video.desc)
-				print("URL = " + "https://tiktok.com/@" + csvuser + "/video/" + str(video.id))
+				# print( video.create_time, video.desc )
+				print( "URL = " + "https://tiktok.com/@" + csvuser + "/video/" + str( video.id ) )
 				fe = fg.add_entry()
-				link = "https://tiktok.com/@" + csvuser + "/video/" + str(video.id)
-				fe.id(link)
+				link = "https://tiktok.com/@" + csvuser + "/video/" + str( video.id )
+				fe.id( link )
 				ts = video.create_time
-				print(ts)
-				fe.published(ts)
-				fe.updated(ts)
-				updated = max(ts, updated) if updated else ts
+				print( ts )
+				fe.published( ts )
+				fe.updated( ts )
+				updated = max( ts, updated ) if updated else ts
 				if video.desc:
-					fe.title(video.desc[0:255])
+					fe.title( video.desc[0:255] )
 				else:
-				  fe.title("No Title")
-				fe.link(href=link)
-				# fe.description("<img src='" + tiktok.as_dict['video']['cover'] + "' />")
+				  fe.title( "No Title" )
+				fe.link( href=link )
+				#fe.description( "<img src='" + tiktok.as_dict['video']['cover'] + "' />" )
 				if video.desc:
-					fe.description(video.desc)
+					fe.description( video.desc )
 				else:
-					fe.description( "No Description")
-				#print(fg.rss_str(pretty=True))
+					fe.description( "No Description" )
+				#print( fg.rss_str( pretty=True ) )
 		
-		fg.updated(updated)
-		fg.atom_file('rss/' + csvuser + '.xml', pretty=True) # Write the RSS feed to a file
+		fg.updated( updated )
+		fg.atom_file( 'rss/' + csvuser + '.xml', pretty=True ) # Write the RSS feed to a file
 	except Exception as e:
-		print(f"-------------------- an error occurred: {e} --------------------")
+		print( f"-------------------- an error occurred: {e} --------------------" )
 		pass
 	
-	print(f'-------------------- run end {csvuser} --------------------')
+	print( f'-------------------- run end {csvuser} --------------------' )
 
 
 
-asyncio.run(runAll())
+asyncio.run( runAll() )
